cc9f1d6cc80be9a1d635adb46859bdb83e83530a
Justin Smestad <justin.smestad@gmail.com>
2021-10-20 16:57:51 -0600

    Remove non-vanilla websocket implementations

--- Package.swift
+++ Package.swift
@@ -13,20 +13,15 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "SwiftPhoenixClient", targets: ["SwiftPhoenixClient"]),
-        .library(name: "StarscreamSwiftPhoenixClient", targets: ["StarscreamSwiftPhoenixClient"]),
-        .library(name: "RxSwiftPhoenixClient", targets: ["RxSwiftPhoenixClient"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMinor(from: "3.1.0")),
+      .package(url: "https://github.com/pusher/NWWebSocket.git",
+               .upToNextMajor(from: "0.5.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "SwiftPhoenixClient", dependencies: []),
-        .target(name: "StarscreamSwiftPhoenixClient", dependencies: ["SwiftPhoenixClient", "Starscream"]),
-        .target(name: "RxSwiftPhoenixClient", dependencies: ["SwiftPhoenixClient", "RxSwift"]),
+        .target(name: "SwiftPhoenixClient", dependencies: ["NWWebSocket"]),
         .testTarget(name: "SwiftPhoenixClientTests", dependencies: ["SwiftPhoenixClient"]),
     ]
 )
